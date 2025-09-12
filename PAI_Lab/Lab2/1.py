554641f9875c6a3ad2afaf3904119a06d22c8101
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2025-09-12 12:21:39 +0500

    Add files via upload

--- PAI_Lab/Lab2/1.py
+++ PAI_Lab/Lab2/1.py
@@ -1,1 +1,27 @@
+password = input("Enter your password: ")
 
+
+has_letter = False
+has_number = False
+has_special = False
+
+
+for char in password:
+    if char.isalpha():
+        has_letter = True
+    if char.isdigit():
+        has_number = True
+    if char in "@#$%":
+        has_special = True
+
+if has_letter and has_number and has_special:
+    print("Valid password")
+else:
+    print("Invalid password")
+
+    if not has_letter:
+        print("- Missing a letter")
+    if not has_number:
+        print("- Missing a number")
+    if not has_special:
+        print("- Missing a special character (@, #, $, %)")
