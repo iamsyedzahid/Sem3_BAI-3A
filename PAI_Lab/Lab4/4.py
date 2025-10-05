8896118cf0b395d696a2cf906317e6ac80c1e66a
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2025-10-05 22:02:04 +0500

    Create 4.py

---
+++ PAI_Lab/Lab4/4.py
@@ -0,0 +1,30 @@
+Q4
+
+class Student:
+def init(self, sid, name):
+self.sid = sid
+self.name = name
+def display(self):
+print("ID: ", self.sid, "Name:", self.name)
+
+class Marks(Student):
+def init(self, sid, name, algo, ds, calc):
+super().init(sid, name)
+self.algo = algo
+self.ds = ds
+self.calc = calc
+def displayMarks(self):
+print("Algo: ", self.algo, "DS:", self.ds, "Calc:", self.calc)
+
+class Result(Marks):
+def init(self, sid, name, algo, ds, calc):
+super().init(sid, name, algo, ds, calc)
+def showResult(self):
+total = self.algo + self.ds + self.calc
+avg = total / 3
+print("Total: ", total, "Average:", avg)
+
+r = Result(1, "Ali", 80, 90, 85)
+r.display()
+r.displayMarks()
+r.showResult()
