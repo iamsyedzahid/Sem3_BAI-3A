d9af4ddf6742c1c74fa85c3fa075162a2227f4e6
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2025-10-05 22:05:26 +0500

    Update 2.py

--- PAI_Lab/Lab4/2.py
+++ PAI_Lab/Lab4/2.py
@@ -1,21 +1,3 @@
-Q1
-
-class Vehicle:
-def init(self, capacity):
-self.capacity = capacity
-def fare(self):
-return self.capacity * 100
-
-class Bus(Vehicle):
-def fare(self):
-total = super().fare()
-return total + (0.1 * total)
-
-b = Bus(50)
-print("Q1 Bus Fare:", b.fare())
-
-
-
 class Shape:
 def area(self):
 pass
