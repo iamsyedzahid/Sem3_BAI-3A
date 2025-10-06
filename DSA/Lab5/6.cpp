211d3990bbf6397590f3b901a95fabae9d40687a
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2025-10-06 20:37:01 +0500

    Update 6.cpp

--- DSA/Lab5/6.cpp
+++ DSA/Lab5/6.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int recursiveArraySum(int* arr[], int sizes[], int dim) {
+    if (dim == 1) {
+        int sum = 0;
+        for (int i = 0; i < sizes[0]; i++) sum += arr[0][i];
+        return sum;
+    }
+    int sum = 0;
+    for (int i = 0; i < sizes[0]; i++)
+        sum += recursiveArraySum((int**)arr[i], sizes + 1, dim - 1);
+    return sum;
+}
+
+int main() {
+    int a1[] = {1, 2};
+    int a2[] = {3, 4, 5};
+    int* arr[] = {a1, a2};
+    int sizes[] = {2, 3};
+    cout << recursiveArraySum(arr, sizes, 2);
+}
