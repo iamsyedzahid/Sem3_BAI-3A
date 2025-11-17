3c51fab18816739705be5d127b529e0c1c77676e
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2025-11-17 19:47:03 +0500

    Create 4.cpp

---
+++ DSA/Lab10/4.cpp
@@ -0,0 +1,16 @@
+void MaxHeapify(int arr[], int n, int i) {
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        MaxHeapify(arr, n, largest);
+    }
+}
