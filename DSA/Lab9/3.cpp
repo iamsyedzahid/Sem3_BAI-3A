c82933dbe29bbfc007cdd7a377503041011ab825
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2025-11-17 19:31:10 +0500

    Create 3.cpp

---
+++ DSA/Lab9/3.cpp
@@ -0,0 +1,16 @@
+/*
+Root: 10
+  Root->Left Child: 5
+      Root->Left->Left Child: 3
+      Root->Left->Right Child: 7
+  Root->Right Child: 15
+      Root->Right->Left Child: 12 (New Node)
+      Root->Right->Right Child: Null
+
+Balance Factors for Each Node: The BF for all nodes is {-1, 0, 1}.
+BF(10) = 0
+BF(15) = 1
+BF(5) = 0
+BF(12, 3, 7) = 0
+Tree Height: The height of the final tree (longest path from the root) is 2 (3 levels).
+*/
