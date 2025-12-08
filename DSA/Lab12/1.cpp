1bfd59cd12056a42afc2fae3b3ff6c6d4fccc995
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2025-12-09 01:37:05 +0500

    Update 1.cpp

--- DSA/Lab12/1.cpp
+++ DSA/Lab12/1.cpp
@@ -1,1 +1,29 @@
+struct SearchResult {
+    vector<int> indices;
+    int totalComparisons = 0;
+};
 
+SearchResult bruteForceSearch(const string& text, const string& pattern) {
+    SearchResult result;
+    int N = text.length();
+    int M = pattern.length();
+    
+    if (M == 0) return result;
+
+    for (int i = 0; i <= N - M; i++) {
+        int j = 0;
+        
+        while (j < M) {
+            result.totalComparisons++;
+            if (text[i + j] != pattern[j]) {
+                break;
+            }
+            j++;
+        }
+        
+        if (j == M) {
+            result.indices.push_back(i);
+        }
+    }
+    return result;
+}
